27d71b16e01d074d8edb4cb4c2efc057fa21bdfc
harshil <harshil@tech.co>
2024-03-10 18:02:53 +0530

    added user schema and password hashing

---
+++ temp-project-nestjs/src/user/constants/user.message.ts
@@ -0,0 +1,11 @@
+export const USER_MESSAGES = {
+  SUCCESS: {
+    SIGNUP: 'User signed up successfully',
+  },
+  ERROR: {
+    INTERNAL_SERVER_ERROR: 'Internal Server Error',
+    ERROR_WHILE_PROCESSING_THE_REQUEST:
+      'Error while processing the request, please try again after some time',
+    REQUIRED_FIELD_IS_MISSING: 'Required field is missing',
+  },
+};
--- temp-project-nestjs/src/user/user.service.ts
+++ temp-project-nestjs/src/user/user.service.ts
@@ -1,4 +1,20 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { User } from './schemas';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  constructor(@InjectModel('users') private readonly user: Model<User>) {}
+
+  async addUser(body): Promise<User> {
+    const user = await this.user.findOne({ firstName: body.firstName });
+    const hashedPassword = user.password;
+
+    const compare = await bcrypt.compare(body.password, hashedPassword);
+
+    const newUser = this.user.create(body);
+    return newUser;
+  }
+}
